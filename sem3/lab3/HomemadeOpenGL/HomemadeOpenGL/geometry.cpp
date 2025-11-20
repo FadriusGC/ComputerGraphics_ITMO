3a0dc9baeaaf172b732d37ccde0d0afbdcbb105e
Daniil Grishin <grishin.daniil.02@gmail.com>
2025-11-21 00:18:47 +0300

    ONO POXIFED
    Боже спасибо Роме, что подсказал убрать эти чертовы <> прикольчики, я без понятия в чем причина и почему оно не компилилось, вероятно там какие-то проблемы с синтаксисом CMake

--- sem3/lab3/HomemadeOpenGL/HomemadeOpenGL/geometry.cpp
+++ sem3/lab3/HomemadeOpenGL/HomemadeOpenGL/geometry.cpp
@@ -7,9 +7,9 @@
 
 template <>
 template <>
-Vec3<int>::Vec3<>(const Vec3<float>& v)
+Vec3<int>::Vec3(const Vec3<float>& v)
     : x(int(v.x + .5)), y(int(v.y + .5)), z(int(v.z + .5)) {}
 
 template <>
 template <>
-Vec3<float>::Vec3<>(const Vec3<int>& v) : x(v.x), y(v.y), z(v.z) {}
+Vec3<float>::Vec3(const Vec3<int>& v) : x(v.x), y(v.y), z(v.z) {}
