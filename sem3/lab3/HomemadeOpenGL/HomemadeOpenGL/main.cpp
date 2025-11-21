01810c5c0344cb84b6d08573770f614adaf2255f
Daniil Grishin <grishin.daniil.02@gmail.com>
2025-11-21 19:16:43 +0300

    Almost finished camera
    Сделал камеру, но пока чет с оутпутом  не очень хорошо

--- sem3/lab3/HomemadeOpenGL/HomemadeOpenGL/main.cpp
+++ sem3/lab3/HomemadeOpenGL/HomemadeOpenGL/main.cpp
@@ -41,139 +41,60 @@ Matrix viewport(int x, int y, int w, int h) {
   return m;
 }
 
-void triangle(Vec3i t0, Vec3i t1, Vec3i t2, Vec2i uv0, Vec2i uv1, Vec2i uv2,
-              TGAImage& image, float intensity, int* zbuffer) {
+void triangle(Vec3i t0, Vec3i t1, Vec3i t2, float ity0, float ity1, float ity2,
+              TGAImage& image, int* zbuffer) {
   if (t0.y == t1.y && t0.y == t2.y) return;
-
   if (t0.y > t1.y) {
     std::swap(t0, t1);
-    std::swap(uv0, uv1);
+    std::swap(ity0, ity1);
   }
   if (t0.y > t2.y) {
     std::swap(t0, t2);
-    std::swap(uv0, uv2);
+    std::swap(ity0, ity2);
   }
   if (t1.y > t2.y) {
     std::swap(t1, t2);
-    std::swap(uv1, uv2);
+    std::swap(ity1, ity2);
   }
 
   int total_height = t2.y - t0.y;
-
   for (int i = 0; i < total_height; i++) {
     bool second_half = i > t1.y - t0.y || t1.y == t0.y;
     int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
     float alpha = (float)i / total_height;
     float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
-
     Vec3i A = t0 + Vec3f(t2 - t0) * alpha;
     Vec3i B =
         second_half ? t1 + Vec3f(t2 - t1) * beta : t0 + Vec3f(t1 - t0) * beta;
-    Vec2i uvA = uv0 + (uv2 - uv0) * alpha;
-    Vec2i uvB =
-        second_half ? uv1 + (uv2 - uv1) * beta : uv0 + (uv1 - uv0) * beta;
-
+    float ityA = ity0 + (ity2 - ity0) * alpha;
+    float ityB =
+        second_half ? ity1 + (ity2 - ity1) * beta : ity0 + (ity1 - ity0) * beta;
     if (A.x > B.x) {
       std::swap(A, B);
-      std::swap(uvA, uvB);
+      std::swap(ityA, ityB);
     }
-
     for (int j = A.x; j <= B.x; j++) {
       float phi = B.x == A.x ? 1. : (float)(j - A.x) / (float)(B.x - A.x);
       Vec3i P = Vec3f(A) + Vec3f(B - A) * phi;
-      Vec2i uvP = uvA + (uvB - uvA) * phi;
-
+      float ityP = ityA + (ityB - ityA) * phi;
       int idx = P.x + P.y * width;
-      if (idx >= 0 && idx < width * height && zbuffer[idx] < P.z) {
+      if (P.x >= width || P.y >= height || P.x < 0 || P.y < 0) continue;
+      if (zbuffer[idx] < P.z) {
         zbuffer[idx] = P.z;
-        TGAColor color = model->diffuse(uvP);
-        image.set(P.x, P.y,
-                  TGAColor(color.r * intensity, color.g * intensity,
-                           color.b * intensity));
+        image.set(P.x, P.y, TGAColor(255, 255, 255) * ityP);
       }
     }
   }
-}
-
-void renderScene(const std::string& output_filename, Camera& camera) {
-  std::cout << "Rendering: " << output_filename << std::endl;
-
-  zbuffer = new int[width * height];
-  for (int i = 0; i < width * height; i++) {
-    zbuffer[i] = std::numeric_limits<int>::min();
-  }
-
-  Matrix ViewPort =
-      viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
-
-  Matrix View = camera.getViewMatrix();
-  Matrix Projection = camera.getProjectionMatrix();
-
-  std::cout << "View matrix created" << std::endl;
-  std::cout << "Projection matrix created" << std::endl;
-
-  TGAImage image(width, height, TGAImage::RGB);
-  std::cout << "TGAImage created: " << width << "x" << height << std::endl;
-
-  for (int i = 0; i < model->nfaces(); i++) {
-    std::vector<int> face = model->face(i);
-    Vec3i screen_coords[3];
-    Vec3f world_coords[3];
-
-    for (int j = 0; j < 3; j++) {
-      Vec3f v = model->vert(face[j]);
-
-      screen_coords[j] = m2v(ViewPort * Projection * v2m(v));
-      world_coords[j] = v;
-    }
-
-    Vec3f n = (world_coords[2] - world_coords[0]) ^
-              (world_coords[1] - world_coords[0]);
-    n.normalize();
-    float intensity = n * light_dir;
-
-    // Back-face culling
-    if (intensity > 0) {
-      Vec2i uv[3];
-      for (int k = 0; k < 3; k++) {
-        uv[k] = model->uv(i, k);
-      }
-      triangle(screen_coords[0], screen_coords[1], screen_coords[2], uv[0],
-               uv[1], uv[2], image, intensity, zbuffer);
-    }
-  }
-
-  image.flip_vertically();
-  image.write_tga_file("output_front.tga");
-
-  // Z-buffer image
-  TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
-  for (int i = 0; i < width; i++) {
-    for (int j = 0; j < height; j++) {
-      int zval = zbuffer[i + j * width];
-      if (zval > std::numeric_limits<int>::min()) {
-        zbimage.set(i, j, TGAColor(zval, 1));
-      }
-    }
-  }
-  zbimage.flip_vertically();
-  zbimage.write_tga_file("zbuffer2.tga");
-
-  delete[] zbuffer;
-  return;
 }
 
 int main(int argc, char** argv) {
   std::cout << "Starting renderer..." << std::endl;
 
-  // Load model
-  const char* model_path = "obj/african_head.obj";
-  if (argc >= 2) {
-    model_path = argv[1];
+  if (2 == argc) {
+    model = new Model(argv[1]);
+  } else {
+    model = new Model("obj/african_head.obj");
   }
-
-  std::cout << "Loading model: " << model_path << std::endl;
-  model = new Model(model_path);
 
   if (model->nverts() == 0) {
     std::cout << "ERROR: Failed to load model!" << std::endl;
@@ -183,13 +104,69 @@ int main(int argc, char** argv) {
   std::cout << "Model loaded: " << model->nverts() << " vertices, "
             << model->nfaces() << " faces" << std::endl;
 
-  float aspect = (float)width / (float)height;
+  zbuffer = new int[width * height];
+  for (int i = 0; i < width * height; i++) {
+    zbuffer[i] = std::numeric_limits<int>::min();
+  }
 
-  std::cout << "\n=== Camera 1: Front view ===" << std::endl;
-  Camera camera1(Vec3f(0, 0, 3), Vec3f(0, 0, 0), Vec3f(0, 1, 0), 45.0f, aspect);
-  renderScene("output_front.tga", camera1);
+  {
+    Vec3f eye(0, 0, 3);
+    Vec3f center(0, 0, 0);
+    Vec3f up(0, 1, 0);
+
+    Camera camera(eye, center, up);
+    Matrix View = camera.getViewMatrix();
+    Matrix Projection = Matrix::identity(4);
+    Projection[3][2] = -1.f / (eye - center).norm();
+    Matrix ViewPort =
+        viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+
+    TGAImage image(width, height, TGAImage::RGB);
+
+    std::cout << "Rendering..." << std::endl;
+    for (int i = 0; i < model->nfaces(); i++) {
+      std::vector<int> face = model->face(i);
+      Vec3i screen_coords[3];
+      Vec3f world_coords[3];
+      float intensity[3];
+
+      for (int j = 0; j < 3; j++) {
+        Vec3f v = model->vert(face[j]);
+        // преобразование: ViewPort * Projection * View * вершина
+        screen_coords[j] = m2v(ViewPort * Projection * View * v2m(v));
+        world_coords[j] = v;
+
+        Vec3f n = (world_coords[2] - world_coords[0]) ^
+                  (world_coords[1] - world_coords[0]);
+        n.normalize();
+        intensity[j] = n * light_dir;
+        if (intensity[j] < 0) intensity[j] = 0;
+        if (intensity[j] > 1) intensity[j] = 1;
+      }
+
+      triangle(screen_coords[0], screen_coords[1], screen_coords[2],
+               intensity[0], intensity[1], intensity[2], image, zbuffer);
+    }
+
+    image.flip_vertically();
+    image.write_tga_file("output.tga");
+    std::cout << "Saved output.tga" << std::endl;
+  }
+
+  {
+    TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        zbimage.set(i, j, TGAColor(zbuffer[i + j * width]));
+      }
+    }
+    zbimage.flip_vertically();
+    zbimage.write_tga_file("zbuffer.tga");
+    std::cout << "Saved zbuffer.tga" << std::endl;
+  }
 
   delete model;
+  delete[] zbuffer;
   std::cout << "Done!" << std::endl;
   return 0;
 }
--- sem3/lab3/HomemadeOpenGL/HomemadeOpenGL/tgaimage.h
+++ sem3/lab3/HomemadeOpenGL/HomemadeOpenGL/tgaimage.h
@@ -2,7 +2,7 @@
 #define __IMAGE_H__
 
 #include <fstream>
-#include <iostream>
+
 #pragma pack(push, 1)
 struct TGA_Header {
   char idlength;
@@ -21,37 +21,41 @@ struct TGA_Header {
 #pragma pack(pop)
 
 struct TGAColor {
-  union {
-    struct {
-      unsigned char b, g, r, a;
-    };
-    unsigned char raw[4];
-    unsigned int val;
-  };
-  int bytespp;
+  unsigned char bgra[4];
+  unsigned char bytespp;
 
-  TGAColor() : val(0), bytespp(1) {}
+  TGAColor() : bgra(), bytespp(1) {
+    for (int i = 0; i < 4; i++) bgra[i] = 0;
+  }
 
   TGAColor(unsigned char R, unsigned char G, unsigned char B,
            unsigned char A = 255)
-      : b(B), g(G), r(R), a(A), bytespp(4) {}
+      : bgra(), bytespp(4) {
+    bgra[0] = B;
+    bgra[1] = G;
+    bgra[2] = R;
+    bgra[3] = A;
+  }
 
-  TGAColor(int v, int bpp) : val(v), bytespp(bpp) {}
+  TGAColor(unsigned char v) : bgra(), bytespp(1) {
+    for (int i = 0; i < 4; i++) bgra[i] = 0;
+    bgra[0] = v;
+  }
 
-  TGAColor(const TGAColor& c) : val(c.val), bytespp(c.bytespp) {}
-
-  TGAColor(const unsigned char* p, int bpp) : val(0), bytespp(bpp) {
-    for (int i = 0; i < bpp; i++) {
-      raw[i] = p[i];
+  TGAColor(const unsigned char* p, unsigned char bpp) : bgra(), bytespp(bpp) {
+    for (int i = 0; i < (int)bpp; i++) {
+      bgra[i] = p[i];
+    }
+    for (int i = bpp; i < 4; i++) {
+      bgra[i] = 0;
     }
   }
 
-  TGAColor& operator=(const TGAColor& c) {
-    if (this != &c) {
-      bytespp = c.bytespp;
-      val = c.val;
-    }
-    return *this;
+  TGAColor operator*(float intensity) const {
+    TGAColor res = *this;
+    intensity = (intensity > 1.f ? 1.f : (intensity < 0.f ? 0.f : intensity));
+    for (int i = 0; i < 4; i++) res.bgra[i] = bgra[i] * intensity;
+    return res;
   }
 };
 
@@ -77,7 +81,8 @@ class TGAImage {
   bool flip_vertically();
   bool scale(int w, int h);
   TGAColor get(int x, int y);
-  bool set(int x, int y, TGAColor c);
+  bool set(int x, int y, TGAColor& c);
+  bool set(int x, int y, const TGAColor& c);
   ~TGAImage();
   TGAImage& operator=(const TGAImage& img);
   int get_width();
